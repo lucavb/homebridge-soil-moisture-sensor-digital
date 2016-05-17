6b5950935b39c121dbac5f3ac6cd564859d4d811
Luca Becker <luca.becker@me.com>
2016-05-17 14:13:33 +0200

    minor bugfixes

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function SoilMoistureSensorDigital(log, config) {
 
     // info service
     this.informationService = new Service.AccessoryInformation();
-       
+
     this.informationService
-    .setCharacteristic(Characteristic.Manufacturer, "Vani")
+    .setCharacteristic(Characteristic.Manufacturer, config.manufacturer ||  "Vani")
     .setCharacteristic(Characteristic.Model, config.model || "YL-69")
     .setCharacteristic(Characteristic.SerialNumber, config.serial || "8C247807-D125-4B36-94D5-36622D49A2FB");
 
@@ -32,12 +32,12 @@ function SoilMoistureSensorDigital(log, config) {
 
     // moisture service
     SoilMoisture = function() {
-       Characteristic.call(this, 'Soil Moisture', 'C160D589-9510-4432-BAA6-5D9D77957138');
-       this.setProps({
-           format: Characteristic.Formats.STRING,
-           perms: [Characteristic.Perms.READ]
-       });
-       this.value = this.getDefaultValue();
+        Characteristic.call(this, 'Soil Moisture', 'C160D589-9510-4432-BAA6-5D9D77957138');
+        this.setProps({
+            format: Characteristic.Formats.STRING,
+            perms: [Characteristic.Perms.READ]
+        });
+        this.value = this.getDefaultValue();
     };
 
     inherits(SoilMoisture, Characteristic);
@@ -45,10 +45,10 @@ function SoilMoistureSensorDigital(log, config) {
     SoilMoisture.UUID = 'C160D589-9510-4432-BAA6-5D9D77957138';
 
     SoilMoistureSensor = function(displayName, subtype) {
-       call(this, displayName, '3C233958-B5C4-4218-A0CD-60B8B971AA0A', subtype);
+        Service.call(this, displayName, '3C233958-B5C4-4218-A0CD-60B8B971AA0A', subtype);
 
-       // Required Characteristics
-       this.addCharacteristic(SoilMoisture);
+        // Required Characteristics
+        this.addCharacteristic(SoilMoisture);
     };
 
     inherits(SoilMoistureSensor, Service);
@@ -65,9 +65,9 @@ SoilMoistureSensorDigital.prototype.getMoisture = function(callback) {
     var that = this;
     setTimeout(function() {
         var val = that.pinRead.readSync();
-        pinVCC.write(0);
-        callback(null, (val == 0) ? "Dry" : "Wet");
-    }, 500);
+        that.pinVCC.write(0);
+        callback(null, (val == 1) ? "Dry" : "Wet");
+    }, 250);
 }
 
 SoilMoistureSensorDigital.prototype.getServices = function() {
